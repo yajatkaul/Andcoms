309ca83be8da389a2c9150d26099149d06609617
YajatKaul <mc.crafter1973@gmail.com>
2024-05-21 14:54:57 +0530

    modified:   lib/main.dart 	modified:   lib/pages/login_page.dart 	new file:   lib/widgets/custom_form_field.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:chatapp/pages/login_page.dart';
 import 'package:chatapp/utils.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() async {
   await setup();
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
       home: LoginPage(),
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:chatapp/widgets/custom_form_field.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -11,23 +12,25 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: _buildUI(),
     );
   }
 
   Widget _buildUI() {
     return SafeArea(
-        child: Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20.0),
-      child: Column(
-        children: [_headerText()],
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20.0),
+        child: Column(
+          children: [_headerText(), _loginForm(), _createAnAccountLink()],
+        ),
       ),
-    ));
+    );
   }
 
   Widget _headerText() {
     return SizedBox(
-      width: MediaQuery.sizeOf(context).width,
+      width: MediaQuery.of(context).size.width,
       child: const Column(
         mainAxisSize: MainAxisSize.max,
         mainAxisAlignment: MainAxisAlignment.start,
@@ -36,6 +39,69 @@ class _LoginPageState extends State<LoginPage> {
           Text(
             "Hi, Welcome Back!",
             style: TextStyle(fontSize: 20, fontWeight: FontWeight.w800),
+          ),
+          Text(
+            "Hello again, you've been missed",
+            style: TextStyle(
+                fontSize: 15, fontWeight: FontWeight.w500, color: Colors.grey),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _loginForm() {
+    return Container(
+      height: MediaQuery.sizeOf(context).height * 0.40,
+      margin: EdgeInsets.symmetric(
+        vertical: MediaQuery.of(context).size.height * 0.05,
+      ),
+      child: Form(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CustomFormField(
+              height: MediaQuery.sizeOf(context).height * 0.1,
+              hintText: "Email",
+            ),
+            CustomFormField(
+              height: MediaQuery.sizeOf(context).height * 0.1,
+              hintText: "Password",
+            ),
+            _loginButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _loginButton() {
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width,
+      child: MaterialButton(
+        color: Theme.of(context).colorScheme.primary,
+        onPressed: () {},
+        child: const Text(
+          "Login",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+  Widget _createAnAccountLink() {
+    return Expanded(
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text("Don't have an account?"),
+          const Text(
+            "Sign Up",
+            style: TextStyle(fontWeight: FontWeight.w800),
           )
         ],
       ),
